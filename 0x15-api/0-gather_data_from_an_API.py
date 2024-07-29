760b3b451a8aa5a74906a928165f6c03e658f5b0
= <=>
2024-07-29 15:21:50 -0700

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
         if todo.get("completed") is True:
             completed.append(todo.get("title"))
 
-    print("Employee {} is done with tasks ({}/{}):".format(
+    print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
 
     for complete in completed:
