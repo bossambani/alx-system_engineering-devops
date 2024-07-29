d9f3bb93b7969ca311d7d9d80958b05aabe6b2c5
= <=>
2024-07-29 14:43:00 -0700

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,23 +4,24 @@ import requests
 import sys
 
 
-url = "https://jsonplaceholder.typicode.com/"
-employee_id = sys.argv[1]
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/"
+    employee_id = sys.argv[1]
 
-user_response = requests.get(url + "/users/{}".format(employee_id))
-user = user_response.json()
+    user_response = requests.get(url + "users/{}".format(employee_id))
+    user = user_response.json()
 
-params = {"userid": employee_id}
-todos_response = requests.get(url +"todos", params=params)
-todos = todos_response.json()
+    params = {"userId": employee_id}
+    todos_response = requests.get(url + "todos", params=params)
+    todos = todos_response.json()
 
-completed = []
-for todo in todos:
-    if todo.get("completed") is True:
-        completed.append(todo.get("title"))
+    completed = []
+    for todo in todos:
+        if todo.get("completed") is True:
+            completed.append(todo.get("title"))
 
-print("Employee {} is done with tasks ({}/{})".format(user.get("name"),
-    len(completed), len(todos)))
+    print("Employee {} is done with tasks ({}/{})".format(
+        user.get("name"), len(completed), len(todos)))
 
-for complete in completed:
-    print("\t {}".format(complete))
+    for complete in completed:
+        print("\t {}".format(complete))
