fcfe9dccc95856b643b49cadd093a48312075e7c
= <=>
2024-07-29 14:26:52 -0700

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+import requests
+import sys
+
+
+url = "https://jsonplaceholder.typicode.com/"
+employee_id = sys.argv[1]
+
+user_response = requests.get(url + "/users/{}".format(employee_id))
+user = user_response.json()
+
+params = {"userid": employee_id}
+todos_response = requests.get(url +"todos", params=params)
+todos = todos_response.json()
+
+completed = []
+for todo in todos:
+    if todo.get("completed") is True:
+        completed.append(todo.get("title"))
+
+print("Employee {} is done with tasks ({}/{})".format(user.get("name"),
+    len(completed), len(todos)))
+
+for complete in completed:
+    print("\t {}".format(complete))
