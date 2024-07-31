abfdbbe84cb372f25463ded26a1f2348f8bc37a4
= <=>
2024-07-31 07:58:35 -0700

    pycodestyle check

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,9 +6,8 @@ import sys
 
 
 if __name__ == '__main__':
-
-    
     url = "https://jsonplaceholder.typicode.com/"
+
     employee_id = sys.argv[1]
     user_response = requests.get(url + "users/{}".format(employee_id))
     user = user_response.json()
