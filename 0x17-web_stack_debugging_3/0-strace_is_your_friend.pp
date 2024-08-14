34715a7996ace0b36c732d0100e371f707e85d93
= <=>
2024-08-14 04:43:26 -0700

    fixing phpp typo

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #Fixing typo in "phpp" instead of "php" in wp-settings.php
 
 exec{'fixing-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',    
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
